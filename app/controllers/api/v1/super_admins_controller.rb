225d090803104cb634d1260185dc7086abcf148b
Kwame Bio <kwameagyemang73@gmail.com>
2023-07-27 10:56:19 +0000

    fixed dashboard issue

---
+++ app/controllers/api/v1/super_admins_controller.rb
@@ -0,0 +1,2 @@
+class API::v1::SuperAdminsController < ApplicationController
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.find_or_create_by!(email: "admin@example.com") do |admin_user|
+    admin_user.password = "password"
+    admin_user.password_confirmation = "password"
+  end
